af7a3e12081e5ec27caa328eb13919cad4918c4f
Keelhauled <keelhauledhs@gmail.com>
2019-12-30 18:20:48 +0200

    Remove Util class

---
+++ PHStudioAnimLoader/PBMessageBox.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace StudioAnimLoader
+{
+    internal class PBMessageBox : MonoBehaviour
+    {
+        private float topOffset = 100f;
+        private Rect rect;
+        private string message;
+        private float time;
+        private bool initialized = false;
+
+        public void Init(float width, float height, float screenWidth, float screenHeight, string message, float time)
+        {
+            rect = new Rect((screenWidth - width) / 2, topOffset, width, height);
+            this.message = message;
+            this.time = time;
+            initialized = true;
+        }
+
+        private void OnGUI()
+        {
+            if (initialized)
+            {
+                GUIStyle guiStyle = new GUIStyle(GUI.skin.textArea);
+                guiStyle.alignment = TextAnchor.MiddleCenter;
+                GUI.Label(rect, message, guiStyle);
+            }
+        }
+
+        private void Start()
+        {
+            Destroy(gameObject, time);
+        }
+    }
+}
